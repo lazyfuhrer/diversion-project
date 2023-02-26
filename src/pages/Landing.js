88724eba5c0e53cf6677647e8a1b2613716c21b7
Ankur Sarkar <78259188+Ankur02Sarkar@users.noreply.github.com>
2023-02-26 06:13:17 +0530

    Update Landing.js

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -69,29 +69,6 @@ function Landing() {
           >
             Get Started
           </Button>
-          <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-            Learn more
-          </Button>
-          <Box>
-            <Icon
-              as={Arrow}
-              color={useColorModeValue("gray.800", "gray.300")}
-              w={71}
-              position={"absolute"}
-              right={-71}
-              top={"10px"}
-            />
-            <Text
-              fontSize={"lg"}
-              fontFamily={"Caveat"}
-              position={"absolute"}
-              right={"-125px"}
-              top={"-15px"}
-              transform={"rotate(10deg)"}
-            >
-              Starting at $15/mo
-            </Text>
-          </Box>
         </Stack>
       </Stack>
     </Container>
