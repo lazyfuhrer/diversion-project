d743ce3f519dcd591984c9344ab8018decb2eb24
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-02-26 12:15:43 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,14 @@
-import { Flex, IconButton, useColorMode, Icon, Input, InputGroup, InputRightElement, Spacer, Box, Button } from "@chakra-ui/react";
+import {
+  Flex, IconButton, useColorMode, Icon, Input, InputGroup, InputRightElement, Spacer, Box, Button, Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure
+} from "@chakra-ui/react";
+import React from "react"
 import { FiMoon, FiSun, FiSearch } from "react-icons/fi";
 import { SearchIcon } from "@chakra-ui/icons";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -12,8 +22,13 @@ function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const { isConnected } = useAccount();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+
   return (
     <Flex
+
       as="nav"
       align="center"
       justify="space-between"
@@ -22,29 +37,65 @@ function Navbar() {
       bg={colorMode === "light" ? "white" : "gray.800"}
       color={colorMode === "light" ? "gray.800" : "white"}
     >
+      <Button ref={btnRef} colorScheme='blue' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+            <Link as={NextLink} href='/'>
+              <Button margin="20px 0px" ref={btnRef} colorScheme='blue' onClick={onClose}>
+                Home
+              </Button></Link>
+            <Link as={NextLink} href='/Upload'>
+              <Button margin="20px 0px" ref={btnRef} colorScheme='blue' onClick={onClose}>
+                Upload
+              </Button></Link>
+            <Link as={NextLink} href='/Feed'>
+              <Button margin="20px 0px" ref={btnRef} colorScheme='blue' onClick={onClose}>
+                Feed
+              </Button></Link>
+            <Link as={NextLink} href='/About'>
+              <Button margin="20px 0px" ref={btnRef} colorScheme='blue' onClick={onClose}>
+                About us
+              </Button></Link>
+          </DrawerBody>
+
+        </DrawerContent>
+      </Drawer>
+
+
       <Flex align="flex-start">
         <Link href="/">
           <Icon as={FiSearch} mr={4} cursor="pointer" />
         </Link>
         {mounted && isConnected && (
-          <InputGroup w={{base: '200px', lg: "400px"}} maxW="400px">
+          <InputGroup w={{ base: '200px', lg: "400px" }} maxW="400px">
             <Input placeholder='Search' />
             <InputRightElement children={<SearchIcon color='gray.500' />} />
           </InputGroup>
         )}
       </Flex>
+
+
+
       <Flex align="center">
-        <Link as={NextLink} href='/Feed'>
-          <Button bg='gray' variant="ghost" mx={{base: '0', sm: '20'}} _hover={{ color: "gray.500" }} color="white">Home</Button>
-        </Link>
-        <Link as={NextLink} href='/Upload'>
-          <Button bg='gray' color={'white'} variant="ghost" _hover={{ color: "gray.500" }} >Upload</Button>
-        </Link>
+
+
       </Flex>
       <Flex justify="flex-end" align="center" flex="1">
         <ConnectButton accountStatus={{
-            smallScreen: 'avatar',
-            largeScreen: 'full',
+          smallScreen: 'avatar',
+          largeScreen: 'full',
         }} />
         <IconButton
           aria-label={`Switch to ${colorMode === "light" ? "dark" : "light"} mode`}
